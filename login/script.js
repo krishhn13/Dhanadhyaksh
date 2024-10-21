89d64a12cf218f6c94427a1b981a6b88697bde28
Krishn <krishan0467.be23@chitkara.edu.in>
2024-10-21 20:40:47 +0530

    login script updated

--- login/script.js
+++ login/script.js
@@ -3,22 +3,65 @@ const loginForm = document.querySelector("form.login");
 const loginBtn = document.querySelector("label.login");
 const signupBtn = document.querySelector("label.signup");
 const signupLink = document.querySelector("form .signup-link a");
+
+
+const signupID = document.getElementById("signup-email");
+const signupPass = document.getElementById("signup-pass");
+const signupPass2 = document.getElementById("signup-conf-pass");
+const security = document.getElementById("security");
+
 signupBtn.onclick = () => {
-  loginForm.style.marginLeft = "-50%";
-  loginText.style.marginLeft = "-50%";
+    loginForm.style.marginLeft = "-50%";
+    loginText.style.marginLeft = "-50%";
 };
+
 loginBtn.onclick = () => {
-  loginForm.style.marginLeft = "0%";
-  loginText.style.marginLeft = "0%";
+    loginForm.style.marginLeft = "0%";
+    loginText.style.marginLeft = "0%";
 };
 
 signupLink.onclick = () => {
-  signupBtn.click();
-  return false;
+    signupBtn.click();
+    return false;
 };
 
-const logininfo = [];
-const pass = [];
+//Lets Sign Up because user is new
+let i = localStorage.getItem('signupIndex') ? parseInt(localStorage.getItem('signupIndex')) : 0;
+function validate(event) {
+    event.preventDefault();
+    if (signupPass.value !== signupPass2.value) {
+        alert("Passwords do not match");
+        return false;
+    } else {
+        localStorage.setItem("email" + i, signupID.value);
+        localStorage.setItem("password" + i, signupPass.value);
+        localStorage.setItem("security"+i,security.value);
+        i++;
+        localStorage.setItem('signupIndex', i);
+        alert("Signup successful!");
+        return true;
+    }
+}
 
-logininfo.push(document.getElementsById("signup-email"));
-pass.push(doucment.getElementsById("signup-pass"));
+
+//Login Function || Checking if the user exists or not 
+function login() {
+    var j = 0;
+    var loginID = document.getElementById("login-email").value;
+    var loginPass = document.getElementById("login-password").value;
+
+    var signupIndex = parseInt(localStorage.getItem('signupIndex')) || 0;
+
+    while (j < signupIndex) {
+        var storedEmail = localStorage.getItem("email" + j);
+        var storedPassword = localStorage.getItem("password" + j);
+
+        if (loginID === storedEmail && loginPass === storedPassword) {
+            window.location.href = "../dashboard/dashboard.html";
+            return; // Exit the function once login is successful
+        }
+        j++;
+    }
+
+    alert("Either Password Or Email ID is Wrong!");
+}
