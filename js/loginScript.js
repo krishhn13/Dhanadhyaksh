64bd07a0a424e4f03d8597dc2bdaed3cf9a6ef0b
Krishan <chiranjiv.shah783@gmail.com>
2025-11-25 19:52:53 +0530

    Update

--- js/loginScript.js
+++ js/loginScript.js
@@ -4,7 +4,6 @@ const loginBtn = document.querySelector("label.login");
 const signupBtn = document.querySelector("label.signup");
 const signupLink = document.querySelector("form .signup-link a");
 
-
 const signupID = document.getElementById("signup-email");
 const signupPass = document.getElementById("signup-pass");
 const signupPass2 = document.getElementById("signup-conf-pass");
